a6b3628de97a950a81de173ec82053e09589a213
Michael McPherson <michael@mpmcpherson.com>
2020-11-07 18:11:05 -0500

    gitScan push

--- gitScan.sh
+++ gitScan.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 workingDir=/home/michaelmcpherson/src;
-#ls $workingDir > 'gitScan2.config';
+ls $workingDir > 'gitScan2.config';
 runDir=$(pwd);
 while read line;
 do
-	cd "$workingDir/$line";
-	echo $(pwd);
-	git add .;
-	git commit -m "gitScan push";
-	git push;
-	#possible password solution
-	#git pull "https://michael@mpmcpherson.com:Adein1Dva2!@github.com/mpmcpherson/<repository_name>.git" <master>
-	#git log;
-	cd $runDir;
-	#echo $line
+	if [ $line != 'notMine' ]
+	then
+		cd "$workingDir/$line";
+		echo $(pwd);
+		git add .;
+		git commit -m "gitScan push";
+		git push;
+		#possible password solution
+		#git pull "https://michael@mpmcpherson.com:Adein1Dva2!@github.com/mpmcpherson/<repository_name>.git" <master>
+		#git log;
+		cd $runDir;
+		#echo $line
+	fi
 done < gitScan2.config
 
 #Basically archival of public repos in a way that checks vs local diffs from prior runs such that even if a git history is altered every change the puller sees is arrived, so if a pull has x new conmits they're all preserved, and if someone hacks the history that's preserved as a separate commit
