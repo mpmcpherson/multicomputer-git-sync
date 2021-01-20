0bae2b3281d17918a3febbdad0696c6326ed4b4d
Michael McPherson <michael@mpmcpherson.com>
2021-01-20 17:17:58 -0500

    autocommit Wed 20 Jan 2021 05:17:58 PM EST

--- gitScan.sh
+++ gitScan.sh
@@ -1,58 +1,21 @@
 #!/bin/bash
-workingDir=/home/michaelmcpherson/src; #fix this so it gets the immediate parent
-#ls $workingDir > 'gitScan2.config';
+workingDir=/home/michaelmcpherson/src;
 runDir=$(pwd);
-echo -n "get, push, or clone? (case and spelling matter): ";
-read;
 
+cd "$workingDir";
+lsVal=$(ls -d */);
+cd $runDir;
 
-if [ ${REPLY} == "get" ]
-then
-	printf "lol we're not there yet \n";
-fi
-
-
-if [ ${REPLY} == "push" ]
-then
-	while read line;
-	do
-		if [ $line != 'notMine' ];
+for line in $lsVal; do
+	if [ $line != 'notMine' ];
+	then
+		cd "$workingDir/$line";
+		if [ $(ls -d .git)=='.git' ];
 		then
-			cd "$workingDir/$line";
-			echo $(pwd);
-			git add .;
-			git commit -m "gitScan push";
+			git add -A;
+			git commit -m "autocommit $(date)";
 			git push;
-			#git config --get remote.origin.url
-			#possible password solution
-			#git pull "https://michael@mpmcpherson.com:Adein1Dva2!@github.com/mpmcpherson/<repository_name>.git" <master>
-			#git log;
-			cd $runDir;
-			#echo $line
 		fi
-	done < gitScan2.config
-fi
-
-if [ ${REPLY} == "clone" ]
-then
-	while read line;
-	do
-		if [ $line != 'notMine' ]
-		then
-			mkdir $line;
-
-			cd "$workingDir/$line";
-			echo $(pwd);
-
-			git clone $
-
-			#git config --get remote.origin.url
-			#possible password solution
-			#git log;
-			cd $runDir;
-			#echo $line
-		fi
-	done < gitScan2.config
-fi
-
-#Basically archival of public repos in a way that checks vs local diffs from prior runs such that even if a git history is altered every change the puller sees is arrived, so if a pull has x new conmits they're all preserved, and if someone hacks the history that's preserved as a separate commit
+		cd $runDir;
+	fi
+done
